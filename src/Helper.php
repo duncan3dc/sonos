7584c6e468f46ad1283fed0ff46b40601dcbb97f
Craig Duncan <git@duncanc.co.uk>
2014-11-07 20:00:08 +0000

    Also return the uri for the album art for tracks

--- src/Helper.php
+++ src/Helper.php
@@ -15,7 +15,7 @@ class Helper extends \duncan3dc\Helpers\Helper
      *
      * @param mixed $xml
      *
-     * @return array Track data containing the following elements (title, atrist, album, track-number)
+     * @return array Track data containing the following elements (title, atrist, album, track-number, album-art)
      */
     public static function getTrackMetaData($xml)
     {
@@ -26,11 +26,17 @@ class Helper extends \duncan3dc\Helpers\Helper
         } else {
             return [];
         }
+
+        if ($album = (string) $parser->getTag("albumArtURI")) {
+            $album = sprintf("http://%s:1400%s", Network::getController()->ip, $album);
+        }
+
         return [
             "title"         =>  (string) $parser->getTag("title"),
             "artist"        =>  (string) $parser->getTag("creator"),
             "album"         =>  (string) $parser->getTag("album"),
             "track-number"  =>  (int)(string) $parser->getTag("originalTrackNumber"),
+            "album-art"     =>  $album,
         ];
     }
 }
