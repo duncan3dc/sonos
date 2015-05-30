de0da5b1b71d732d1c887e00d48c7806f570bb66
Craig Duncan <git@duncanc.co.uk>
2015-05-30 14:41:08 +0100

    Allow the title of stream to be retrieved

---
+++ tests/Tracks/StreamTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace duncan3dc\Sonos\Test\Tracks;
+
+use duncan3dc\Sonos\Tracks\Stream;
+
+class StreamTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetUri()
+    {
+        $stream = new Stream("uri-test");
+        $this->assertSame("uri-test", $stream->getUri());
+    }
+
+
+    public function testGetTitle1()
+    {
+        $stream = new Stream("uri-test");
+        $this->assertSame("", $stream->getTitle());
+    }
+
+
+    public function testGetTitle2()
+    {
+        $stream = new Stream("uri-test", "super stream");
+        $this->assertSame("super stream", $stream->getTitle());
+    }
+
+
+    public function testGetMetaData1()
+    {
+        $stream = new Stream("uri-test");
+
+        $xml = '<DIDL-Lite ';
+            $xml .= 'xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" ';
+            $xml .= 'xmlns:dc="http://purl.org/dc/elements/1.1/" ';
+            $xml .= 'xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" ';
+            $xml .= 'xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/">';
+            $xml .= '<item id="-1" parentID="-1" restricted="true">';
+                $xml .= '<dc:title>Stream</dc:title>';
+                $xml .= '<upnp:class>object.item.audioItem.audioBroadcast</upnp:class>';
+                $xml .= '<desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">SA_RINCON65031_</desc>';
+            $xml .= '</item>';
+        $xml .= '</DIDL-Lite>';
+        $this->assertSame($xml, $stream->getMetadata());
+    }
+    public function testGetMetaData2()
+    {
+        $stream = new Stream("uri-test", "super stream");
+
+        $xml = '<DIDL-Lite ';
+            $xml .= 'xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" ';
+            $xml .= 'xmlns:dc="http://purl.org/dc/elements/1.1/" ';
+            $xml .= 'xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" ';
+            $xml .= 'xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/">';
+            $xml .= '<item id="-1" parentID="-1" restricted="true">';
+                $xml .= '<dc:title>super stream</dc:title>';
+                $xml .= '<upnp:class>object.item.audioItem.audioBroadcast</upnp:class>';
+                $xml .= '<desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">SA_RINCON65031_</desc>';
+            $xml .= '</item>';
+        $xml .= '</DIDL-Lite>';
+        $this->assertSame($xml, $stream->getMetadata());
+    }
+}
